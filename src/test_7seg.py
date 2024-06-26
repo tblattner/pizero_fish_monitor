51ec438376623eb72264ccb42e85699982a24b91
tblattner <tblattner@gmail.com>
2024-06-25 23:32:11 -0400

    Added test code to show temperature

--- src/test_7seg.py
+++ src/test_7seg.py
@@ -13,34 +13,39 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create the LED segment class.
 # This creates a 7 segment 4 character display:
 display = segments.Seg7x4(i2c)
+display.brightness = 0.25
+#display.colon(turn_on=False)
+display[4] = 'F'
+
+display.print('{}F'.format(98.6))
 
 # Clear the display.
-display.fill(0)
+#display.fill(0)
 
 # Can just print a number
-display.print(42)
-time.sleep(1)
+#display.print(42)
+#time.sleep(1)
 
 # Set the first character to '1':
-display[0] = '1'
+#display[0] = '1'
 # Set the second character to '2':
-display[1] = '2'
+#display[1] = '2'
 # Set the third character to 'A':
-display[2] = 'A'
+#display[2] = 'A'
 # Set the forth character to 'B':
-display[3] = 'B'
-time.sleep(1)
+#display[3] = 'B'
+#time.sleep(1)
 
-numbers = [0.0, 1.0, 0.55, 10.23, 100.5]
+#numbers = [0.0, 1.0, 0.55, 10.23, 100.5]
 
 # print floating point numbers
-for i in numbers:
-    display.print(str(i))
-    time.sleep(0.5)
+#for i in numbers:
+#    display.print(str(i))
+#    time.sleep(0.5)
 
 # print hex values, enable colon
-for i in range(0xFF):
-    display.fill(0)
-    display.print(':')
-    display.print(hex(i))
-    time.sleep(0.25)
+#for i in range(0xFF):
+#    display.fill(0)
+#    display.print(':')
+#    display.print(hex(i))
+#    time.sleep(0.25)
